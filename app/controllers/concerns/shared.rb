ff72096e8c0dcdb5072ce012975ce9d1ab23287b
Austine Amah <austine.amah@gmail.com>
2022-01-24 15:59:42 +0100

    Set default country to nigeria

--- app/controllers/concerns/shared.rb
+++ app/controllers/concerns/shared.rb
@@ -2,7 +2,13 @@ module Shared
   extend ActiveSupport::Concern
 
   def country_by_ip
-    local = "102.165.19.19"
+    # 129.56.85.93 niger
+    # 84.17.36.91
+    # 197.251.212.242 Ghana?
+    # 102.140.226.63 Uganda
+    # 102.165.19.19 South Africa
+
+    local = "129.56.85.93"
     db = MaxMindDB.new('./GeoLite2-Country.mmdb')
 
     request_ip = request.remote_ip == "::1" ? local : request.remote_ip == "127.0.0.1" ? local : request.remote_ip
