1119ef7610e1f552a367463da51d2077561ec040
Austine Amah <austine.amah@gmail.com>
2022-01-27 15:18:19 +0100

    Revert lesson show template

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -2,17 +2,29 @@ class LessonsController < ApplicationController
   before_action :set_lesson, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user, only: [:destroy, :create]
 
-    def create
-      course = Course.find_by(slug: params[:slug])
-      @lesson = course.lessons.build(lesson_params)
-      @lesson.user = current_user
-  
-      if @lesson.save
-        render json: :show, status: :created
-      else
-        render json: @lesson.errors, status: :unprocessable_entity
-      end
+  def create
+    course = Course.find_by(slug: params[:slug])
+    @lesson = course.lessons.build(lesson_params)
+    @lesson.user = current_user
+
+    if @lesson.save
+      v = polymorphic_url(@lesson.video) if @lesson.video.attached?
+      lc = polymorphic_url(@lesson.cover.variant(Lesson.large_options)) if @lesson.cover.attached?
+      lt = polymorphic_url(@lesson.cover.variant(Lesson.thumbnail_options)) if @lesson.cover.attached?
+
+      lesson = {
+        title: @lesson.title,
+        video: v,
+        cover: lc,
+        thumbnail: lt,
+        locked: false
+      }
+      
+      render json: lesson, status: :created
+    else
+      render json: @lesson.errors, status: :unprocessable_entity
     end
+  end
   
     def show
     end
